5e013a3a466c8e345f5cc57fca1f24a565cc7f6d
Abdullah9001 <abdullahjubayer44@gmail.com>
2023-05-28 01:22:13 +0600

    some change on project 2

--- project_2/app.js
+++ project_2/app.js
@@ -14,7 +14,7 @@ function main() {
   });
 }
 
-function rgbColorGenerate() {
+function hexaColorGenerate() {
   const red = Math.floor(Math.random() * 255) + 1;
   const green = Math.floor(Math.random() * 255) + 1;
   const blue = Math.floor(Math.random() * 255) + 1;
---
+++ project_3/app.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+  main();
+};
+
+function main() {}
+
+function rgbColorGenerate() {}
