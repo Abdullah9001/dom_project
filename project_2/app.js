379598266b71fe5667905969e9d3aae7b5da85c0
Abdullah9001 <abdullahjubayer44@gmail.com>
2023-05-28 01:11:31 +0600

    done second project

--- project_2/app.js
+++ project_2/app.js
@@ -2,4 +2,26 @@ window.onload = () => {
   main();
 };
 
-function main() {}
+function main() {
+  const bg = document.getElementById("root");
+  const button = document.getElementById("cng-color");
+  const output = document.getElementById("output");
+
+  button.addEventListener("click", function () {
+    const bgColor = rgbColorGenerate();
+    bg.style.backgroundColor = bgColor;
+    output.value = bgColor;
+  });
+}
+
+function rgbColorGenerate() {
+  const red = Math.floor(Math.random() * 255) + 1;
+  const green = Math.floor(Math.random() * 255) + 1;
+  const blue = Math.floor(Math.random() * 255) + 1;
+
+  const redHex = red.toString(16).padStart(2, "0");
+  const greenHex = green.toString(16).padStart(2, "0");
+  const blueHex = blue.toString(16).padStart(2, "0");
+
+  return `#${redHex}${greenHex}${blueHex}`;
+}
