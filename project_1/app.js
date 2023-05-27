c265560db951b5e5c367b5c261974e864f3f5dcc
Abdullah9001 <abdullahjubayer44@gmail.com>
2023-05-27 23:10:30 +0600

    first dom project done, rgb color change

---
+++ project_1/app.js
@@ -0,0 +1,20 @@
+window.onload = () => {
+  main();
+};
+
+function main() {
+  const bg = document.getElementById("root");
+  const button = document.getElementById("cng-color");
+
+  button.addEventListener("click", function () {
+    const bgColor = rgbColorGenerate();
+    bg.style.backgroundColor = bgColor;
+  });
+}
+
+function rgbColorGenerate() {
+  const red = Math.floor(Math.random() * 225) + 1;
+  const green = Math.floor(Math.random() * 225) + 1;
+  const blue = Math.floor(Math.random() * 225) + 1;
+  return `rgb(${red}, ${green}, ${blue})`;
+}
